c6240335d419644dda2009350f4353da77f32af7
alcarinquenessima <106439733+alcarinquenessima@users.noreply.github.com>
2024-07-04 20:46:35 +0300

    Update Main.java

--- src/main/java/be/ma/Main.java
+++ src/main/java/be/ma/Main.java
@@ -2,7 +2,7 @@ package be.ma;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("good day");
+        System.out.println("good");
         int RAAA;
     }
 }
