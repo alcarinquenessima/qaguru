82c415ebcc08b33caffb7a11eac1b43baddd1ae4
Rasty <alcarinquenessima@gmail.com>
2024-07-04 20:37:38 +0300

    fixed new message in Main

--- src/main/java/be/ma/Main.java
+++ src/main/java/be/ma/Main.java
@@ -2,7 +2,7 @@ package be.ma;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("fffffff);
-        System.out.printl!!!!!!!!!!!\n");
+        System.out.println("fffffff");
+        System.out.println("!!!!!!999!\n");
     }
 }
