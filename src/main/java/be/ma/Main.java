75d2e13af79d26b827a2ec65fe4a22b31e846266
Rasty <alcarinquenessima@gmail.com>
2024-07-04 20:39:25 +0300

    experiments

--- src/main/java/be/ma/Main.java
+++ src/main/java/be/ma/Main.java
@@ -2,5 +2,19 @@ package be.ma;
 
 public class Main {
     public static void main(String[] args) {
+        aaa
+                a
+                aaa
+                        a
+
+                        aa
+                                a
+                                aa
+                                        a
+
+
+
+
+                                        jjjj
     }
 }
